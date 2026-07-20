49fbc7acb55a47ae9b963f9c8609fb2acc4e8bda
DagarovAbduaziz <dagarovabduaziz108@gmail.com>
2026-07-20 14:29:43 +0500

    new model

--- furniture_guard/detector.py
+++ furniture_guard/detector.py
@@ -46,7 +46,7 @@ CONFIG = {
         # 4: "mashina",
     },
 
-    "confidence_threshold":   0.70,
+    "confidence_threshold":   0.75,
     "alert_cooldown_seconds": 40,
     "frame_delay_ms": 1,
 
